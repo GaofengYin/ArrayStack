3227b7572d28d66b924220876abe8d88386711be
Gaofeng <gaofengyin@hotmail.com>
2019-06-04 11:20:37 +0100

    ArrayStack

---
+++ tp2_36233_36232/src/Stack/ArrayStack.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stack;
+
+/**
+ *
+ * @author gaofeng
+ */
+public class ArrayStack<E> implements Stack<E> {
+
+    private static final int CAPACITY = 1000;
+    private E[] data;
+    private int t = -1;
+
+    public ArrayStack() {
+        this(CAPACITY);
+    }
+
+    public ArrayStack(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    public int size() {
+        return (t + 1);
+    }
+
+    public boolean isEmpty() {
+        return (t == -1);
+    }
+
+    public void push(E e) throws IllegalStateException {
+        if (size() == data.length) {
+            throw new IllegalStateException("Stack is full");
+        }
+        data[++t] = e;
+    }
+
+    public E top() {
+        if (isEmpty()) {
+            return null;
+        }
+        return data[t];
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        E answer = data[t];
+        data[t] = null;
+        t--;
+        return answer;
+    }
+}
