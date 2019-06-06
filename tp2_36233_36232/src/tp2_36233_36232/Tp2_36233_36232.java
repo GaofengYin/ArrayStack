e238a5cba9062f4952f4961e57687b609bd7ffbf
Francisco Azevedo <franck_extrela@hotmail.com>
2019-06-06 16:35:27 +0100

    já funca

--- tp2_36233_36232/src/tp2_36233_36232/Tp2_36233_36232.java
+++ tp2_36233_36232/src/tp2_36233_36232/Tp2_36233_36232.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package tp2_36233_36232;
+
 import Stack.ArrayStack;
 import Stack.Stack;
+
 /**
  *
  * @author Francisco José Silva de Azevedo , a36232 , EI
@@ -13,11 +15,52 @@ import Stack.Stack;
  */
 public class Tp2_36233_36232 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String verificaIntigridade(String str) {
+
+        Stack<Character> stack = new ArrayStack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch == '[' || ch == '(' || ch == '{') {
+
+                stack.push(ch);
+
+            } else if ((ch == ']' || ch == '}' || ch == ')') && (!stack.isEmpty())) {
+
+                if (((char) stack.top() == '(' && ch == ')') || ((char) stack.top() == '{' && ch == '}') || ((char) stack.top() == '[' && ch == ']')) {
+                    stack.pop();
+                } else {
+                    return "False";
+                }
+            } else {
+
+                if ((ch == ']' || ch == '}' || ch == ')')) {
+
+                    return "False";
+
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return "True";
+        } else {
+            return "False";
+        }
     }
-    
+
+    public static void main(String[] args) {
+        String[] str = {"([{}])","(["};
+        int i;
+        if (str == null) {
+            System.out.println("Empty String");
+            return;
+        }
+        for(i=0; i<2; i++){
+            System.out.println("Integridade dos parêntesis " + str[i]+ " ? : " + verificaIntigridade(str[i]));
+        } 
+        
+    }
+
 }
