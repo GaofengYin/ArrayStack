b17e50e2f82e0344622728a7b001c0ed9bf7b0e2
Francisco Azevedo <franck_extrela@hotmail.com>
2019-06-07 19:06:53 +0100

    melhoras

--- tp2_36233_36232/src/tp2_36233_36232/Tp2_36233_36232.java
+++ tp2_36233_36232/src/tp2_36233_36232/Tp2_36233_36232.java
@@ -19,7 +19,7 @@ public class Tp2_36233_36232 {
 
     /**
      * Metodo para verificar integridade dos parenteses.
-     * Este metodo cria um arraystack que guarda os '(','{'e'['.Com ciclo for percorrermos o string e ao encontrar o carater '(' ou '{' ou '[' faz o stack.push(inic).
+     * Este metodo cria um arraystack que guarda os '(','{'e'['. Com o ciclo for percorremos a string e ao encontrar o carater '(' ou '{' ou '[' faz o stack.push(inic).
      * Quando encontrar ')' ou '}' ou ']' faz o stack.top() para  ver o objeto que esta no topo da lista e verificar se ha integridade com o parentese, caso haja faz o stack.pop() que 
      * remover o objeto que esta no topo da lista.
      * @param str string para verificar a integridade
@@ -57,7 +57,7 @@ public class Tp2_36233_36232 {
      */
     public static void main(String[] args) {
 
-        //Array de string onde se vai verificar se ha integridade
+        //Array de string onde se vai verificar se há integridade
         String str[] = {"(abcd)", "{f>{e>{d>{a<b}}", "{7+{6-{5+{2+2}}})", "{{b{ana}}na}(m)", "{j{d{m}d}](m)", "{{}}([]){}{}{}{}{[[[[]]]]}"};
         int i;
         //Verificar se o array esta vazio
@@ -65,7 +65,7 @@ public class Tp2_36233_36232 {
             System.out.println("String esta vazia.");
             return;
         }
-        //ciclo for para percorrer o array de string e verifica se ha integridade
+        //ciclo for para percorrer o array de string e verifica se há integridade
         for (i = 0; i < str.length; i++) {
             System.out.println("Integridade dos parêntesis " + str[i] + " ? : " + verificaIntigridade(str[i]));
         }
